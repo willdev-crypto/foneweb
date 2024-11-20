232b85202976884eda366a51e8634200d90561d3
willdev-crypto <wil.dgl@hotmail.com>
2024-11-20 10:52:13 -0300

    CONFORMIDADE E FORMATAÇÃO DE CODIGO

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-console.log('Aqui teste de console no navegad e tudo funcionando ok');
+console.log('Aqui teste de console no navegador e tudo funcionando ok');
 document.getElementById("cpf-cnpj").addEventListener("input", function (event) {
     const input = event.target;
     const value = input.value;
 
-    // Remove qualquer caractere que não seja número
+    
     input.value = value.replace(/\D/g, "");
 
-    // Validação extra (opcional) para exibir mensagem de erro
+    
     const errorElement = document.getElementById("cpf-error");
     if (/\D/.test(value)) {
         errorElement.style.display = "block";
